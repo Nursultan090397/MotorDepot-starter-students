a676b9139e4a84f94b2890ef5ca6e7514e2538b0
osorov97 <osorov.nursultan97@gmail.com>
2022-10-07 17:04:12 +0600

    task22

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.service.ServiceImpl;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -9,32 +10,62 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
-
+    final static Scanner scannerS = new Scanner(System.in);
+    final static Scanner scannerN = new Scanner(System.in);
+    final static Scanner scannerC = new Scanner(System.in);
+    final static Scanner scannerB = new Scanner(System.in);
     public static final GsonBuilder BUILDER = new GsonBuilder();
     public static final Gson GSON = BUILDER.setPrettyPrinting().create();
     public static final Path WRITE_PATH = Paths.get("./truck.json");
     public static final Path WRITE_PATH1 = Paths.get("./driver.json");
 
     public static void main(String[] args) throws Exception {
-        buttons();
+        ServiceImpl service = new ServiceImpl();
+
+        String command = "null";
+
+        while (!command.equals("x")) {
+            service.TrucksView();
+            System.out.println("Choose truck: (INTEGER)");
+            int truckID = scannerB.nextInt();
+            service.searchTruck(truckID);
+
+            buttons();
+            command = scannerC.nextLine();
+            switch (command) {
+                case "1" -> {
+                    service.changeDriver(truckID);
+                    service.seeDrivers();
+                }
+                case "2" -> service.startDriving(truckID);
+                case "3" -> service.startRepair(truckID);
+                default -> System.out.println("Нету такой кнопки");
+            }
+        }
     }
 
     public static void buttons(){
+        System.out.println("\n---------------METHODS--------------------");
         System.out.println("Press 1 to change Driver\n" +
-        "Press 2 to send to the Route\n" +
-        "Press 3 to send to the Repairing\n");
+                "Press 2 to send to the Route\n" +
+                "Press 3 to send to the Repairing\n" +
+                "Press 4 to change truck state to Base");
+        System.out.println("-----------------------------------");
+        System.out.println("Choose command:");
     }
 
 
-   public static String readTtuck() {
-       return getString(WRITE_PATH);
-   }
+    public static String readTtuck() {
+        return getString(WRITE_PATH);
+    }
 
-   public static String readDriver() {
-       return getString(WRITE_PATH1);
-   }
+    public static String readDriver() {
+        return getString(WRITE_PATH1);
+    }
 
     private static String getString(Path writePath1) {
         StringBuilder json = new StringBuilder();
--- src/com/company/service/ServiceImpl.java
+++ src/com/company/service/ServiceImpl.java
@@ -2,10 +2,13 @@ package com.company.service;
 
 
 import com.company.entities.Driver;
+import com.company.entities.State;
 import com.company.entities.Truck;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 import static com.company.Main.*;
 
@@ -18,31 +21,118 @@ public class ServiceImpl implements Service{
         return trucks;
     }
 
+    public void TrucksView(){
+        System.out.println("#  |Truck              |State         |   Driver   ");
+
+        for (Truck t: trucks) {
+            System.out.println(t.getId() + "  |  " + t.getTruckName() +"         |  " + t.getState() + "      |" + t.getDriver());
+        }
+    }
+
+    public void seeDrivers(){
+
+        System.out.println();
+
+        for (Driver d: drivers) {
+            System.out.println(d.getIdDiver() + "  |  " + d.getName() + "  |  " + d.getTruckName());
+        }
+
+        System.out.println("------------------------------------");
+    }
+
     public List<Driver> getDrivers() {
         return drivers;
     }
 
-    @Override
-    public void changeDriver(int truckId) {
+    public void searchTruck(int ID){
+        System.out.println("\n------------TRUCK INFORMATION---------------");
+        Optional<Truck> optional = trucks.stream().filter(x -> x.getId() == ID).findFirst();
 
+        if (optional.isPresent()) optional.ifPresent(x -> System.out.println("ID: " + x.getId() + "\nTrack name: " + x.getTruckName() + "\nDriver: " + x.getDriver() + "\nState: " + x.getState()));
+        else System.out.println("Трак не найден");
     }
 
     @Override
-    public void startDriving(int truckId) {
+    public void changeDriver(int truckId) {
+        for (Truck x : trucks) {
+            if (x.getId() == truckId && x.getState() == State.BASE) {
+                int counter = 0;
+                for (Driver d : drivers) {
+                    if (d.getTruckName().equals(x.getTruckName())){
+                        d.setTruckName("null");
+                    }
 
+                    if (d.getTruckName().equals("") && counter == 0) {
+                        counter++;
+                        x.setDriver(d.getName());
+                        d.setTruckName(x.getTruckName());
+                    }
+
+                    if (d.getTruckName().equals("null")){
+                        d.setTruckName("");
+                    }
+                }
+            }
+
+            if (x.getId() == truckId && x.getState() == State.REPAIR){
+                System.out.println("Нельзя сменить водителя");
+            }
+        }
+    }
+
+
+    @Override
+    public void startDriving(int truckId) {
+        Random random = new Random();
+        for (int i = 0; i < getTrucks().size(); i++) {
+            if (truckId == getTrucks().get(i).getId()){
+                if (getTrucks().get(i).getState() == State.REPAIR){
+                    int num = random.nextInt(3);
+                    switch (num){
+                        case 1 -> {
+                            getTrucks().get(i).setState(State.BASE);
+                            System.out.println("Трак на базе");
+                        }
+                        case 2 -> {
+                            getTrucks().get(i).setState(State.ROUTE);
+                            System.out.println("Трак в пути");
+                        }
+                    }
+                }
+
+                if (getTrucks().get(i).getState() == State.ROUTE){
+                    System.out.println("ERROR: Грузовик уже в пути");
+                }
+
+                if (getTrucks().get(i).getState() == State.BASE){
+                    getTrucks().get(i).setState(State.ROUTE);
+                    System.out.println("------Грузовик в пути------");
+                }
+            }
+        }
     }
 
     @Override
     public void startRepair(int truckId) {
+        Optional<Truck> optional = trucks.stream().filter(x -> x.getId() == truckId && x.getState() != State.REPAIR).findFirst();
 
+        if (optional.isPresent()) {
+            optional.ifPresent(x -> x.setState(State.REPAIR));
+            System.out.println("-----Трак в ремонте------");
+        } else System.out.println("Трак уже на ремонте");
     }
 
     @Override
-    public void changeTruckState() {
+    public void changeTruckState(int truckId) {
+        Optional<Truck> optional = trucks.stream().filter(x -> x.getId() == truckId).findFirst();
 
+        if (optional.isPresent()) {
+            optional.ifPresent(x -> x.setState(State.BASE));
+            System.out.println("-----Трак на базе------");
+        }
+        else System.out.println("Трак не найден");
     }
 }
-
 
 
 
